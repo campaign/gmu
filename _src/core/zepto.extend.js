9352f6eab135725b1551f2acdbf26e59f7a0295f
chenluyang <gavincly@gamil.com>
2012-12-05 09:16:39 +0800

    add $.location, $.fn.animateFrom, modified ajax.js $.ajaxJSONP

--- _src/core/zepto.extend.js
+++ _src/core/zepto.extend.js
@@ -263,7 +263,21 @@
         debounce: function(delay, fn, t) {
             return fn === undefined ? $.throttle(250, delay, false) : $.throttle(delay, fn, t === undefined ? false : t !== false);
         },
-
+        /**
+         * @desc 调用地图API，获取当前地址位置详细信息
+         * **successCB：** 获取信息成功回调函数
+         * **errorCB：** 获取信息失败回调函数
+         * **options:**
+         * - ***enableHighAccuracy***: boolean 是否要求高精度的地理信息
+         * - ***timeout***: 获取信息的超时限制
+         * - ***maximumAge***: 对地理信息进行缓存的时间
+         *
+         * @grammar $.location(fn, fn, options) ⇒ function
+         * @name $.location
+         * @example $.location(function(rs){
+         *      console.log(rs)
+         *  })
+         */
         location : function(successCB, errorCB, options){
             //获取地图提供的js api
             $.ajaxJSONP({
