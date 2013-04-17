0bbae843f47c6e3360affd8e0f26ef54c1044161
zhoumm1008 <zhoumeimei@baidu.com>
2013-04-17 15:21:03 +0800

    [bugfix] add setTimeout for example.js

--- _examples/example.js
+++ _examples/example.js
@@ -156,7 +156,7 @@
             }
             setTimeout(function () {
                 $(document).trigger('headready');
-            }, 0);
+            }, 100);
         }
     }).initHeader();
 
